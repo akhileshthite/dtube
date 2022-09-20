2ccc9e10e0dbeb3bd5ba02e47f850bf365fa5efb
Akhilesh Thite <akhileshthite@hotmail.com>
2022-09-20 19:07:44 +0530

    fix: 'ethereum.enable()' is deprecated

--- src/components/App.js
+++ src/components/App.js
@@ -30,7 +30,9 @@ class App extends Component {
   async loadWeb3() {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum);
-      await window.ethereum.enable();
+      await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
     } else if (window.web3) {
       window.web3 = new Web3(window.web3.currentProvider);
     } else {
