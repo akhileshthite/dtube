0f8e2c5ae15f10fbd6544e82635919275714211e
Akhilesh Thite <akhileshthite@hotmail.com>
2022-09-21 00:59:11 +0530

    feat: deploy to ethereum mainnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,21 @@
-require('babel-register');
-require('babel-polyfill');
-require('dotenv').config();
-const HDWalletProvider = require("@truffle/hdwallet-provider")
+require("babel-register");
+require("babel-polyfill");
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*"
+      network_id: "*",
     },
     matic: {
-      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://rpc-mumbai.maticvigil.com/v1/bd76d9f2e2c3993bbfef0082f25c30d017fc1feb`),
+      provider: () =>
+        new HDWalletProvider(
+          process.env.REACT_APP_MNEMONIC,
+          `https://rpc-mumbai.maticvigil.com/v1/bd76d9f2e2c3993bbfef0082f25c30d017fc1feb`
+        ),
       network_id: 80001,
       confirmations: 2,
       timeoutBlocks: 200,
@@ -19,50 +23,61 @@ module.exports = {
       gas: 6000000,
       gasPrice: 10000000000,
     },
+    mainnet: {
+      provider: function () {
+        return new HDWalletProvider(
+          process.env.REACT_APP_MNEMONIC,
+          process.env.REACT_APP_INFURA_MAINNET
+        );
+      },
+      network_id: 1,
+      // gas: 6000000,
+      // gasPrice: 10000000000,
+    },
     rinkeby: {
       provider: function () {
         return new HDWalletProvider(
-          process.env.MNEMONIC,
-          process.env.INFURA_RINKEBY
-        )
+          process.env.REACT_APP_MNEMONIC,
+          process.env.REACT_APP_INFURA_RINKEBY
+        );
       },
       network_id: 4,
       gas: 6000000,
       gasPrice: 10000000000,
     },
     ropsten: {
-      provider: function() {
+      provider: function () {
         return new HDWalletProvider(
-          process.env.MNEMONIC,
-          process.env.INFURA_ROPSTEN
-        )
+          process.env.REACT_APP_MNEMONIC,
+          process.env.REACT_APP_INFURA_ROPSTEN
+        );
       },
       gas: 5000000,
       gasPrice: 25000000000,
-      network_id: "*"
+      network_id: "*",
     },
     goerli: {
       provider: function () {
         return new HDWalletProvider(
-          process.env.MNEMONIC,
-          process.env.INFURA_GOERLI
-        )
+          process.env.REACT_APP_MNEMONIC,
+          process.env.REACT_APP_INFURA_GOERLI
+        );
       },
       network_id: 5,
       networkCheckTimeout: 1000000,
       timeoutBlocks: 200,
       gas: 5000000,
-      gasPrice: 25000000000
+      gasPrice: 25000000000,
     },
   },
-  contracts_directory: './src/contracts/',
-  contracts_build_directory: './src/abis/',
+  contracts_directory: "./src/contracts/",
+  contracts_build_directory: "./src/abis/",
   compilers: {
     solc: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
-  }
-}
+        runs: 200,
+      },
+    },
+  },
+};
